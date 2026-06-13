d9dfe656993c31e67b3457c6369bdc74c62342d4
Aviral <uncleprosk@gmail.com>
2026-06-13 20:37:05 +0530

    songs list UI structure update

--- src/app/(main)/liked/page.js
+++ src/app/(main)/liked/page.js
@@ -1,13 +1,37 @@
 import SongList from '@/components/ui/SongList'
 import React from 'react'
 
+const likedSongs = [
+  {
+    id: 1,
+    title: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    duration: 355, // in seconds
+    thumbnail: '',
+  },
+  {
+    id: 2,
+    title: 'Are we still friends',
+    artist: 'Tyler the creator',
+    duration: 355,
+    thumbnail: '',
+  },
+  {
+    id: 3,
+    title: 'Ye shaam mastani',
+    artist: 'Kishore kumar',
+    duration: 355,
+    thumbnail: '',
+  },
+]
+
 const LikedPage = () => {
   return (
     <SongList
      listName="Liked Page"
      listAuthor="Aviral"
      listCover="/song-cover.png"
-     songsList={[]}/>
+     songsList={likedSongs}/>
   )
 }
 
--- src/components/ui/SongList.js
+++ src/components/ui/SongList.js
@@ -1,10 +1,11 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 
 const SongList = ({ listName, listAuthor, listCover, songsList }) => {
   return (
     <div className='mr-4'>
-        <div className='flex w-full h-40 gap-10'>
+        <div className='flex w-full h-40 gap-10 bg-potify-surface'>
             <Image
              src={listCover}
              alt='ok'
@@ -19,19 +20,32 @@ const SongList = ({ listName, listAuthor, listCover, songsList }) => {
         </div>
 
 
-        <div className='mt-4 grid grid-cols-[50px_minmax(0,2fr)_minmax(0,2fr)_1fr_50px]'>
-            <p>S.no</p>
+        <div className='mt-4 grid grid-cols-[50px_minmax(0,2fr)_minmax(0,2fr)_1fr_50px_10px] opacity-30'>
+            <p className='truncate'>S.no</p>
             <p className='truncate'>Title</p>
-            <p className='truncate'>Album</p>
-            <p>Duration</p>
+            <p className='truncate'>Artist</p>
+            <p className='truncate'>Duration</p>
+            <p className='truncate'>cover</p>
         </div>
-        <div className='w-full h-[1px] bg-white'></div>
+        <div className='w-full h-[1px] bg-gray-500 opacity-30'></div>
 
-        {songsList.map((song) => (
-            <div key={song.id}>
-
-            </div>
-        ))}
+        {songsList.map((song) => {
+            return (
+                <div key={song.id} className='grid grid-cols-[50px_minmax(0,2fr)_minmax(0,2fr)_1fr_50px] py-2'>
+                    <p className='truncate'>{song.id}.</p>
+                    <p className='truncate'>{song.title}</p>
+                    <p className='truncate'>{song.artist}</p>
+                    <p className='truncate'>{song.duration}</p>
+                    <Image
+                        src={song.thumbnail || '/song-cover.png'}
+                        alt=''
+                        width={20}
+                        height={20}
+                        className='h-auto rounded object-cover'
+                        onError={(e) => e.target.src = '/song-cover.png'}
+                        />
+                </div>
+        )})}
     </div>
   )
 }

cee1072a1a16ed24ce352f28f3cd187db3682570
Aviral <uncleprosk@gmail.com>
2026-06-13 19:35:44 +0530

    fixed layout bug related to html structure

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,10 +1,7 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+
         <main>{children}</main>
-        <footer className="text-xs"></footer>
-      </body>
-    </html>
+
   )
 }
